51111476674135b188a7ab3198a720c3eff933f0
asrahman97 <asrahman97@gmail.com>
2019-12-14 20:41:37 -0500

    added basic layout of homepage

--- wedding-app/src/App.js
+++ wedding-app/src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        July 25, 2020
+        #insertCornyHashtagHere
+
+        <nav className='header'>
+          <h3>RSVP</h3>
+          <h3>Photos</h3>
+          <h3>Registry</h3>
+        </nav>
+
+        <div>
+          <img src='' alt='img goes here' />
+        </div>
+
+        <input>
+        </input>
+
+        <div className='votes'>
+          Where all the guest divs will stay!
+        </div>
+
+
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
