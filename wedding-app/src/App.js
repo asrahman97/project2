978fdc44d6532490132dcaf3c14e4666508c7e6b
asrahman97 <asrahman97@gmail.com>
2019-12-19 10:14:31 -0500

    cleaned more code

--- wedding-app/src/App.js
+++ wedding-app/src/App.js
@@ -42,93 +42,9 @@ class App extends Component {
   }
 
 
-  // toggleConfirmation = (indexToChange) => 
-  //   this.setState({
-  //     guests: this.state.guests.map((guest, index) => {
-  //       if (index === indexToChange) {
-  //         return {
-  //           ...guest,
-  //           confirmed: !guest.confirmed
-  //         }
-  //       }
-  //       return guest
-  //     })
-  //   })
   
-  
-  // toggleGuestProperty = (property, indexToChange) => 
-  //   this.setState({
-  //     guests: this.state.guests.map((guest, index) => {
-  //       if (index === indexToChange) {
-  //         return {
-  //           ...guest,
-  //         [property]: !guest[property]
-  //         }
-  //       }
-  //       return guest
-  //     })
-  //   })
-  
-  
-  // toggleConfirmation = (index) =>
-  //   this.toggleGuestProperty('confirmed', index)
-  
-  // toggleEditing = (index) =>
-  //   this.toggleGuestProperty('editing', index)
-  
-  
-  // removeGuest = (index) => 
-  //   this.setState({
-  //     guests: [
-  //       ...this.state.guests.splice(0, index),
-  //       ...this.state.guests.splice(index + 1)
-  //     ]
-  //   })
-  
-  
-  //   setEditWithIndex = (name, indexToChange) => 
-  //   this.setState({
-  //     guests: this.state.guests.map((guest, index) => {
-  //       if (index === indexToChange) {
-  //         return {
-  //           ...guest,
-  //         name
-  //         }
-  //       }
-  //       return guest
-  //     })
-  //   })
-  
-
-  // handleNameInput = (e) => 
-  //   this.setState({
-  //     inputGuest: e.target.value
-  //   })
-  
-  // newGuest = (e) => {
-  //   e.preventDefault();
-  //     this.setState({
-  //       guests: [
-  //         {
-  //           name: this.state.inputGuest,
-  //           confirmed: false,
-  //           editing: false
-  //         },
-  //         ...this.state.guests
-  //       ],
-  //       inputGuest: ''
-        
-  //     })
-  // }
-
-  // totalInvited = () => this.state.guests.length;
-  // confirmedGuests = () => this.state.guests.reduce((confirm, guest) => guest.confirmed ? confirm +1: confirm, 0)
 
   render() {
-
-    // const total = this.totalInvited();
-    // const numberConfirmed = this.confirmedGuests();
-    // const numberUnconfirmed = (total - numberConfirmed);
 
     return (
       <div className="App">
