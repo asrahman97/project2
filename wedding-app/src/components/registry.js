9578ff2e8c3a9237a13daa1bb08896fc4e27700c
asrahman97 <asrahman97@gmail.com>
2019-12-19 15:48:46 -0500

    added registry cards

--- wedding-app/src/components/registry.js
+++ wedding-app/src/components/registry.js
@@ -1,36 +1,24 @@
-import React, { Component } from 'react';
-import RegistryItems from './registryItems'
-
+import React, { Component } from "react";
+import RegistryItems from "./registryItems";
 
 class Registry extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    
-    }
+    this.state = {};
   }
-
 
   render() {
     return (
-      <div>
-      
-      
-        <RegistryItems
-        />
-  
-  
-  
-  
-  
-    </div>
-    )
-  }
+      <div className="reg-card">
+        <RegistryItems body="testing passing props" />
 
+        <RegistryItems body="testing passing props" />
+
+        <RegistryItems body="testing passing props" />
+      </div>
+    );
+  }
 }
 
-
-
-
-export default Registry
+export default Registry;
--- wedding-app/src/components/registryItems.js
+++ wedding-app/src/components/registryItems.js
@@ -5,12 +5,13 @@ const Image =
   "https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg";
 const Price = "$1,000";
 
-const RegistryItems = () => {
+const RegistryItems = (props) => {
   return (
     <div className='reg-grid'>
       <h2>{Title}</h2>
-      <img src={Image} />
+      <img src={Image} alt='gift image' className='reg-image' />
       <p>{Price}</p>
+      <p>{props.body}</p>
     </div>
   );
 };
