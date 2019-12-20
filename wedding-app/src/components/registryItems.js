77b1117c8a1d46d582c9dc8e6e24072a6a4c0fb8
asrahman97 <asrahman97@gmail.com>
2019-12-20 09:18:43 -0500

    added another card

--- wedding-app/src/components/registryItems.js
+++ wedding-app/src/components/registryItems.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Title = ["Honeymoon Fund", "Kitchen Appliances", "Sofa"];
+const Title = ["Honeymoon Fund", "Kitchen Appliances", "Sofa", "Puppy Fund"];
 const Image = [
   "https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg",
   "https://www.grundig.com/ktchnmag/wp-content/uploads/2018/07/GRUNDIG_KTCHN_kitchen-appliance-trends-2018_GRUNDIG.jpg",
-  "https://okl.scene7.com/is/image/OKL/091916_sofa_Lead?wid=1000&op_sharpen=1"
+  "https://okl.scene7.com/is/image/OKL/091916_sofa_Lead?wid=1000&op_sharpen=1", "https://miro.medium.com/max/519/1*QSSCg3fhfWiwXWqV_uZcEg.jpeg"
 ];
 
-const Price = ["$1,000", "$300", "$400"];
+const Price = ["$1,000", "$300", "$400", "$500"];
 
-const Body =["We've always dreamed of relaxing on the beaches of Thailand! With your contributions, we'll be able to make that a reality after the wedding", "We're huge fans of cooking and experimenting with new recipes. We're also health nuts so please make sure these are BPA-Free haha", "Although we love being social and going on adventures, sometimes ya just gotta relax at home and watch a movie"]
+const Body =["We've always dreamed of relaxing on the beaches of Thailand! With your contributions, we'll be able to make that a reality after the wedding", "We're huge fans of cooking and experimenting with new recipes. We're also health nuts so please make sure these are BPA-Free haha", "Although we love being social and going on adventures, sometimes ya just gotta relax at home and watch a movie", "Contribute to this fund to help us with adopting a new puppy!"]
 
 const RegistryItems = props => {
   return (
@@ -34,6 +34,8 @@ const RegistryItems = props => {
         <p>{Price[2]}</p>
         <p>{Body[2]}</p>
       </div>
+
+
     </div>
   );
 };
